ddaa6bd2e2239a684bc14dd13b6a192a576db54d
Adil Ashurov <ashurovadil96@gmail.com>
2024-11-06 20:13:56 +0400

    change dep

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,13 +5,14 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      redisUrl: process.env.REDIS_URL,
+      
       workerMode: process.env.MEDUSA_WORKER_MODE,
     }
   },
